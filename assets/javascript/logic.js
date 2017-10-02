85616449d077074f138b7acfb0b222307e2c7dad
Zaynaib <zaynaibg@gmail.com>
2017-10-01 22:40:55 -0500

    got the gifs working. The dom manipulations needs work.

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -4,9 +4,23 @@
 
 //create an array of animals
 var animalArray =["hamster","goat","dog"];
-//then add buttons to the dom of thepage
 
-//get the value of text input from the user
+
+function getUserInput(){
+	var userInput= $("#animal-input").val().trim();
+	console.log(userInput);
+	return userInput;
+
+}
+
+//put the users input into a query string
+function getQueryString(str){
+	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+    str + "&api_key=dc6zaTOxFJmzC&limit=5";
+    console.log(queryURL);
+    return queryURL;
+
+}
 
 //make a create button function
 function createButton(arr){
@@ -25,7 +39,7 @@ createButton(animalArray);
 
 //var userInput;
 
-//adds a button to the animal div
+//adds a button to the animal div when the sumbit button is clicked
 $("#addAnimal").on("click",function(event){
 	event.preventDefault();
 	var userInput= getUserInput();
@@ -36,10 +50,11 @@ $("#addAnimal").on("click",function(event){
 
 });
 
-//action for new animaButtons
-
+//action for new animalButtons
+function giphInfo(){
 $(".giphy").on("click",function(){
-	
+	//empty the gifs div so animals can be replace everytime button is clicked
+	$("#gifs").empty();
 	var myAnimal = $(this).attr("data-name");
 	console.log(myAnimal);
 
@@ -55,15 +70,23 @@ $(".giphy").on("click",function(){
         .done(function(response) {
           // Storing an array of results in the results variable
           var results = response.data;
-          console.log(results);
+          //console.log(results);
+          var animalDiv = $("<div id=giphyAnimalsDiv>");
+          //console.log(results.length);
+
           for(var i=0;i<results.length; i++){
-          	//var animalDiv = $()
+          	//animalDiv.empty();
+
           	var animalImage = $("<img>");
-          	animalImage.attr('src',results[i].images.fixed_height.url);
+          	animalImage.attr('src',results[i].images.fixed_height_still.url);
           	animalImage.attr("data-state", "still");
-          	$("#gifs").empty();
-          	$("#gifs").append(animalImage);
+          	animalImage.attr("data-still", results[i].images.fixed_height_still.url);
+          	animalImage.attr("data-animate", results[i].images.fixed_height.url);
+          	animalImage.addClass("animalGifs");
+          	$("#giphyAnimalsDiv").append(animalImage);
+          	
           }
+          $("#gifs").append(animalDiv);
 
           //get the still images
           //get the action images
@@ -71,55 +94,31 @@ $(".giphy").on("click",function(){
 	
 
 });
+}
+// Adding a click event listener to all elements with a class of "giphy"
+$(document).on("click", ".giphy", giphInfo);
 
 
+//action for the giphs active and still states
+
+function clickedAnimals(){
+$(".animalGifs").on("click", function(){
+	var gifState = $(this).attr("data-state");
+	console.log(gifState);
+	if (gifState === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+      } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+      }
+    });
+}
+$(document).on("click", ".animalGifs", clickedAnimals);
 
 
-
-
-
-
-
-
-
-	//add 5 images to the dom of giphys animals
+ 
 	
-	//var stillImage = 
-
-	//when the giphy images are clicked it becomes animated
-
-
-function animalButtons(){
-
-}
-
-function animalImages(){
-
-}
-
-//have to use the this keyword. 
-//every time the user hits of an but of class of giphy animal
-//it will call to the api
-
-
-
-
-//get the users input
-function getUserInput(){
-	var userInput= $("#animal-input").val().trim();
-	console.log(userInput);
-	return userInput;
-
-}
-
-//put the users input into a query string
-function getQueryString(str){
-	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-    str + "&api_key=dc6zaTOxFJmzC&limit=3";
-    console.log(queryURL);
-    return queryURL;
-
-}
 
 
 
@@ -127,35 +126,14 @@ function getQueryString(str){
 
 
 
-  
-//after getting the input call to the api
-//append the button
-//show gifs
-//clear gifs
+
+
+
+
+
 
 
  
 
 
 
-//make an api call request
-/*
-function ajaxRequest(queryString){
-      // Performing our AJAX GET request
-      $.ajax({
-          url: queryString,
-          method: "GET"
-        })
-        // After the data comes back from the API
-        .done(function(response) {
-          // Storing an array of results in the results variable
-          var results = response.data;
-          //console.log(results);
-          return results;
-
-          //get the still images
-          //get the action images
-      });
-      
-  }
-    */
